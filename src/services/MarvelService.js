a62b3dc65fda9297996512b4f2143d683aae23f3
Artur Radchenko <kisharchi@gmail.com>
2024-08-13 22:44:37 +0200

    add pagination

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -2,6 +2,7 @@
 class MarvelService {
   _apiBase = 'https://gateway.marvel.com:443/v1/public/';
   _apiKey = 'apikey=14af6c22f72197bd0bbf41812179fbb7';
+  _baseOffset = 210;
 
   getResources = async(url) => {
     let res = await fetch(url);
@@ -13,8 +14,8 @@ class MarvelService {
     return await res.json();
   }
 
-  getAllCharacters = async () => {
-    const res = await this.getResources(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+  getAllCharacters = async (offset = this._baseOffset) => {
+    const res = await this.getResources(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
     return res.data.results.map(this._transformCharacter);
   }
 
